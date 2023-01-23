d9c4255d399e37436288f1c3666eab192b0f6dc8
sampannap12 <56804514+sampannap12@users.noreply.github.com>
2023-01-23 14:11:17 +0000

    added firstore database
    when new user is created new document will be created it in the firestore and users detail will be added

--- code/app/src/main/java/com/nfclab/e_leap_project/MainActivity.java
+++ code/app/src/main/java/com/nfclab/e_leap_project/MainActivity.java
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText()
+
     }
 
 
--- code/app/src/main/java/com/nfclab/e_leap_project/Register.java
+++ code/app/src/main/java/com/nfclab/e_leap_project/Register.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -15,16 +16,26 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
+    public static final String TAG = "TAG";
     EditText mFullName, mEmail, mPassword;
     CheckBox mAccountype;
     Button mRegisterBtn;
     Button mLoginBtn;
     FirebaseAuth fAuth;
+    FirebaseFirestore fstore;
+    String userID;
 
 
     @Override
@@ -35,11 +46,12 @@ public class Register extends AppCompatActivity {
         mFullName = findViewById(R.id.fullName);
         mEmail = findViewById(R.id.email);
         mPassword =findViewById(R.id.password);
-        mAccountype = findViewById(R.id.checkBox);
+        mAccountype = (CheckBox) findViewById(R.id.checkBox);
         mRegisterBtn =findViewById(R.id.registerButton);
         mLoginBtn =findViewById(R.id.loginButton);
 
         fAuth = FirebaseAuth.getInstance();
+        fstore =FirebaseFirestore.getInstance();
         // To check if the user is already logged in if so sent them to the main activity
         if (fAuth.getCurrentUser() != null)
         {
@@ -52,6 +64,17 @@ public class Register extends AppCompatActivity {
             String email = mEmail.getText().toString().trim();
             String password = mPassword.getText().toString().trim();
             String fullname = mFullName.getText().toString().trim();
+            boolean mAccountypeChecked = mAccountype.isChecked();
+            float balance = 0;
+            String AccountType;
+            if (mAccountypeChecked == true)
+            {
+                AccountType = "Student";
+            }
+            else
+            {
+                AccountType= "Adult";
+            }
 
             if(TextUtils.isEmpty(email)){
                 mEmail.setError("Email is Required");
@@ -80,6 +103,19 @@ public class Register extends AppCompatActivity {
                     //if successful
                     if (task.isSuccessful()){
                         Toast.makeText(Register.this, "User Created.", Toast.LENGTH_SHORT).show();
+                        userID = fAuth.getCurrentUser().getUid();
+                        DocumentReference documentReference = fstore.collection("users").document(userID);
+                        Map<String, Object> user = new HashMap<>();
+                        user.put("Full_Name",fullname );
+                        user.put("Email", email);
+                        user.put("Account Type", AccountType);
+                        user.put("Balance", balance);
+                        documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Log.d(TAG, "profile created  " + userID);
+                            }
+                        });
                         startActivity(new Intent(getApplicationContext(), MainActivity.class));
                     }
                     else
