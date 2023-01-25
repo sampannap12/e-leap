c7c3012ab4532f0e049361e2119786c88bc71178
sampannap12 <56804514+sampannap12@users.noreply.github.com>
2023-01-25 14:14:52 +0000

    added bottom navigation
    navigation added and all the fragments added need to fix the layout of individual fragment and  modify main

--- code/app/src/main/java/com/nfclab/e_leap_project/MainActivity.java
+++ code/app/src/main/java/com/nfclab/e_leap_project/MainActivity.java
@@ -1,15 +1,20 @@
 package com.nfclab.e_leap_project;
 
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -17,16 +22,24 @@ import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener
+{
     TextView user_name,balance,card_num,account_type;
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
     String userID;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnItemSelectedListener(this);
+
+        loadFragment(new Dashboard_Fragment());
+
         user_name = findViewById(R.id.name);
         balance = findViewById(R.id.Balance);
         card_num = findViewById(R.id.card_num);
@@ -62,5 +75,33 @@ public class MainActivity extends AppCompatActivity {
         FirebaseAuth.getInstance().signOut(); //log out user
         startActivity(new Intent(getApplicationContext(),Login.class));
         finish();
+    }
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+            switch (item.getItemId()) {
+                case R.id.dashboard:
+                    fragment = new Dashboard_Fragment();
+                    break;
+                case R.id.profile:
+                    fragment = new Profile();
+                    break;
+                case R.id.topup:
+                    fragment = new Topup();
+                    break;
+                case R.id.routes:
+                    fragment = new Routes();
+                    break;
+
+            }
+            if (fragment != null) {
+                loadFragment(fragment);
+            }
+            return true;
+        }
+
+    void loadFragment(Fragment fragment) {
+        //to attach fragment
+        getSupportFragmentManager().beginTransaction().replace(R.id.constrain_layout, fragment).commit();
     }
 }
