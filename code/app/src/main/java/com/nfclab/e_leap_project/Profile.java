987494839489574ec87642f2bba312db38858ef4
sampannap12 <56804514+sampannap12@users.noreply.github.com>
2023-02-03 15:11:33 +0000

    email change
    email change in authentication

--- code/app/src/main/java/com/nfclab/e_leap_project/Profile.java
+++ code/app/src/main/java/com/nfclab/e_leap_project/Profile.java
@@ -1,24 +1,27 @@
 package com.nfclab.e_leap_project;
 
-import android.content.Intent;
+import static android.os.SystemClock.sleep;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
@@ -29,59 +32,65 @@ import com.google.firebase.firestore.Transaction;
 
 public class Profile extends Fragment {
 
-    public static TextView txt_status ;
-    EditText etemail,etname;
-
-
+    EditText etemail, etname;
     Button button;
     FirebaseFirestore fstore = FirebaseFirestore.getInstance();
     DocumentReference documentReference;
+    EditText password;
 
-    Button btn_pay ;
+    private Context mContext;
+
+    public Profile() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mContext = context;
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
     }
 
-
+    @SuppressLint("CutPasteId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view= (View) inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = (View) inflater.inflate(R.layout.fragment_profile, container, false);
         etname = (EditText) view.findViewById(R.id.edit_full_name);
         etemail = (EditText) view.findViewById(R.id.edit_email);
+        password = (EditText) view.findViewById(R.id.edit_email);
 
-        button =view.findViewById(R.id.save);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                updateProfile();
-            }
+        button = view.findViewById(R.id.save);
+        button.setOnClickListener(v -> {
+            updateProfile();
         });
         return view;
     }
 
+
     public void onStart() {
         super.onStart();
-        FirebaseUser user= FirebaseAuth.getInstance().getCurrentUser();
-        String currentID= user.getUid();
-        documentReference= fstore.collection("users").document(currentID);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String currentID = user.getUid();
+        documentReference = fstore.collection("users").document(currentID);
         documentReference.get()
                 .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<DocumentSnapshot> task) {
 
-                        if(task.getResult().exists()){
+                        if (task.getResult().exists()) {
 
-                            String nameResult= task.getResult().getString("Full_Name");
-                            String AccountResult= task.getResult().getString("Account_Type");
+                            String nameResult = task.getResult().getString("Full_Name");
+                            String AccountResult = task.getResult().getString("Account_Type");
                             Double balanceResult = task.getResult().getDouble("Balance");
                             String balanc = Double.toString(balanceResult);
-                            String emailResult= task.getResult().getString("Email");
-
-
+                            String emailResult = task.getResult().getString("Email");
 
                             etname.setText(nameResult);
                             etemail.setText(emailResult);
@@ -96,9 +105,10 @@ public class Profile extends Fragment {
 
         String name = etname.getText().toString();
         String email = etemail.getText().toString();
+        String pass = password.getText().toString();
         FirebaseFirestore fstore = FirebaseFirestore.getInstance();
-        FirebaseUser user= FirebaseAuth.getInstance().getCurrentUser();
-        String currentID= user.getUid();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String currentID = user.getUid();
 
         final DocumentReference sDoc = fstore.collection("users").document(currentID);
         fstore.runTransaction(new Transaction.Function<Void>() {
@@ -115,14 +125,32 @@ public class Profile extends Fragment {
                     @Override
                     public void onSuccess(Void aVoid) {
 
+                        Toast.makeText(mContext, "Updated", Toast.LENGTH_SHORT).show();
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(mContext, "Error Updating", Toast.LENGTH_SHORT).show();
 
                     }
                 });
+
+
+        // Get auth credentials from the user for re-authentication
+        AuthCredential credential = EmailAuthProvider.getCredential(email, pass);
+
+        assert user != null;
+        user.updateEmail(email).addOnCompleteListener(task1 -> {
+            if (task1.isSuccessful()) {
+                sleep(150);
+                Toast.makeText(mContext, "Email  updated ", Toast.LENGTH_SHORT).show();
+            } else {
+                sleep(160);
+                Toast.makeText(mContext, "Error updating profile info (email) ", Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
     }
 }
