021b2081ea55dd3bf92d984ec96441ee4a043986
sampannap12 <56804514+sampannap12@users.noreply.github.com>
2023-02-23 01:00:00 +0000

    Updated ResetPass
    Bug fixed now user can reset their password using  forget password

--- code/app/src/main/java/com/nfclab/e_leap_project/Resetpass.java
+++ code/app/src/main/java/com/nfclab/e_leap_project/Resetpass.java
@@ -3,12 +3,14 @@ package com.nfclab.e_leap_project;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -22,7 +24,8 @@ import com.google.firebase.firestore.FirebaseFirestore;
 public class Resetpass extends AppCompatActivity {
     EditText mEmail;
     Button mButton;
-    FirebaseAuth auth = FirebaseAuth.getInstance();
+    Context context1 = this;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,28 +33,26 @@ public class Resetpass extends AppCompatActivity {
         setContentView(R.layout.activity_resetpass);
         mEmail = findViewById(R.id.Reset_email);
         mButton = findViewById(R.id.reset_button);
-
+        Toast.makeText(this, "Email" + mEmail, Toast.LENGTH_SHORT).show();
 
         mButton.setOnClickListener(v -> {
-            passwordReset();
+            String email = mEmail.getText().toString().trim();
+
+            FirebaseAuth.getInstance().sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(context1, "Password Reset Email Sent to "+ email, Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        Toast.makeText(context1, "Error Sending password reset ", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
         });
     }
 
 
-    private void passwordReset() {
-
-        auth.sendPasswordResetEmail(String.valueOf(mEmail))
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if (task.isSuccessful()) {
-                            Toast.makeText(Resetpass.this, "Email Sent", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), Login.class));
-                        }
-                    }
-                });
-
-    }
     }
 
 
